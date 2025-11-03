d72452ce77f2efeec20e86382a798f6fc0f63c82
FanetheDivine <1974269607@qq.com>
2025-11-03 14:28:41 +0000

    pwd 基础

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
+import createPWAPlugin from '@ducanh2912/next-pwa'
 import nextAnalyzer from '@next/bundle-analyzer'
 
 const nextConfig: NextConfig = {
   output: process.env.EXPORT === 'true' ? 'export' : undefined,
 }
 
+const withPWA = createPWAPlugin({
+  dest: 'public',
+})
 const withNextIntl = createNextIntlPlugin()
 const withAnalyzer = nextAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
-export default withAnalyzer(withNextIntl(nextConfig))
+export default withAnalyzer(withPWA(withNextIntl(nextConfig)))
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { hasLocale } from 'next-intl'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getTranslations, setRequestLocale } from 'next-intl/server'
@@ -12,22 +12,60 @@ import AntdProvider from '@/lib/AntdProvider'
 import SWRProvider from '@/lib/SWRProvider'
 
 export async function generateMetadata(props: LocaleParams): Promise<Metadata> {
-  // 为了适应打包为静态html的情况
   const { params } = props
   const { locale } = await params
-  const t = await getTranslations({ locale })
+  setRequestLocale(locale)
+  const t = await getTranslations()
+  const app = t('metadata.app')
+  const app_default_title = t('metadata.app_default_title')
+  const app_title_template = `%s - ${app}`
+  const desc = t('metadata.desc')
   return {
-    title: t('metadata.title'),
+    icons: '/logo.svg',
+    applicationName: app,
+    title: {
+      default: app_default_title,
+      template: app_title_template,
+    },
+    description: desc,
+    manifest: '/manifest.json',
+    appleWebApp: {
+      capable: true,
+      statusBarStyle: 'default',
+      title: app_default_title,
+    },
+    formatDetection: {
+      telephone: false,
+    },
+    openGraph: {
+      type: 'website',
+      siteName: app,
+      title: {
+        default: app_default_title,
+        template: app_title_template,
+      },
+      description: desc,
+    },
+    twitter: {
+      card: 'summary',
+      title: {
+        default: app_default_title,
+        template: app_title_template,
+      },
+      description: desc,
+    },
   }
 }
 
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF',
+}
+
 const RootLayout: FC<PropsWithChildren> = async (props) => {
-  // 不能放在FC的类型里面 过不了检查
   const { children, params } = props as PropsWithChildren & LocaleParams
 
   const locale = await match(process.env.EXPORT === 'true')
     .with(true, async () => {
-      // 静态html
       const { locale } = await params
       return locale
     })
