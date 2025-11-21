7c9e3801e18d0faa48d588233d8008c8c051c808
Fane <141594291+FanetheDivine@users.noreply.github.com>
2025-11-21 10:25:53 +0800

    Active Audio (#3)
    * action type
    
    * useAudioList
    
    * add set type
    
    * active audio
    
    ---------
    
    Co-authored-by: Fane <godwokenfane@gmail.com>

--- src/hooks/useAudioList/index.ts
+++ src/hooks/useAudioList/index.ts
@@ -1,3 +1,4 @@
+import { match } from 'ts-pattern'
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import { GoogleAudio } from '@/utils'
@@ -12,21 +13,23 @@ type AudioListActionMap = {
   /** 直接设置 */
   set: GoogleAudio[]
 }
+
+type ActiveAudioActionMap = {
+  next: undefined
+  prev: undefined
+  set: GoogleAudio
+}
+
 export type AudioListStore = {
   audioList: GoogleAudio[]
-  dispatchAudioList: (
-    action: {
-      [K in keyof AudioListActionMap]: {
-        type: K
-        value: AudioListActionMap[K]
-      }
-    }[keyof AudioListActionMap],
-  ) => void
+  activeAudio?: GoogleAudio
+  dispatchAudioList: (action: ActionType<AudioListActionMap>) => void
+  dispatchActiveAudio: (action: ActionType<ActiveAudioActionMap>) => void
 }
 
 export const useAudioList = create(
   persist<AudioListStore>(
-    (set) => {
+    (set, get) => {
       return {
         audioList: [],
         dispatchAudioList: (action) => {
@@ -82,6 +85,40 @@ export const useAudioList = create(
             default:
               throw new Error('未知的action类型')
           }
+        },
+        dispatchActiveAudio: (action) => {
+          if (action.type === 'set') {
+            set({
+              activeAudio: action.value,
+            })
+            return
+          }
+          const { audioList, activeAudio } = get()
+          if (audioList.length === 0) {
+            set({
+              activeAudio: undefined,
+            })
+            return
+          }
+          const currentIndex = audioList.findIndex((item) => item.id === activeAudio?.id)
+          if (currentIndex === -1) {
+            set({
+              activeAudio: audioList[0],
+            })
+            return
+          }
+          set({
+            activeAudio: match(action)
+              .with(
+                { type: 'next' },
+                () => audioList[currentIndex === audioList.length - 1 ? 0 : currentIndex + 1],
+              )
+              .with(
+                { type: 'prev' },
+                () => audioList[currentIndex === 0 ? audioList.length - 1 : currentIndex - 1],
+              )
+              .exhaustive(),
+          })
         },
       }
     },
