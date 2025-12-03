dd479feaddcb888a8438f27071b2a33ffde6926d
Fane <godwokenfane@gmail.com>
2025-12-03 01:26:19 +0000

    type

--- src/app/[locale]/manifest.json/route.ts
+++ src/app/[locale]/manifest.json/route.ts
@@ -1,16 +1,18 @@
+import type { MetadataRoute } from 'next'
 import { hasLocale } from 'next-intl'
 import { getTranslations } from 'next-intl/server'
 import { notFound } from 'next/navigation'
-import { type NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { routing } from '@/i18n/routing'
 
-export async function GET(req: NextRequest, ctx: RouteContext<'/[locale]/manifest.json'>) {
-  const { locale } = await ctx.params
+export async function GET(req: NextRequest, context: RouteContext<'/[locale]/manifest.json'>) {
+  const params = await context.params
+  const { locale } = params
   if (!hasLocale(routing.locales, locale)) {
     notFound()
   }
   const t = await getTranslations({ locale })
-  return NextResponse.json({
+  return NextResponse.json<MetadataRoute.Manifest>({
     name: t('metadata.app'),
     short_name: t('metadata.short_name'),
     icons: [
